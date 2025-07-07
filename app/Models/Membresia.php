3f0a4d2380d0323c28f74ca0dd8f9e1917470afa
Mariano <marianogiardinamonti@gmail.com>
2025-07-07 16:41:27 -0300

    Recupero commit, no le des bola

---
+++ app/Models/Membresia.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Membresia extends Model
+{
+    /** @use HasFactory<\Database\Factories\MembresiaFactory> */
+    use HasFactory;
+}
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('rol')->default(0); // 0: user, 1: profesor, 2: admin
+            $table->integer('fecha_nacimiento'); // Edad del usuario(input date)
+            $table->integer('altura')->nullable(); // Altura del usuario, opcional
+            $table->integer('peso')->nullable(); // Peso del usuario, opcional
+            $table->integer('celular')->nullable(); // Celular del usuario, opcional
+            
+            $table->rememberToken();//Esto es para remember me
             $table->timestamps();
         });
 
