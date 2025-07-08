55399eaeb649fa5c0fa0fc0fb77c282f1b425111
Mariano <marianogiardinamonti1@gmail.com>
2025-07-07 23:03:15 -0300

    Add comment for x-head configuration in index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,5 @@
 <x-head></x-head>
+{{-- -- //x-headimportante, tiene la config de tailwind y los colores del gym -- --}}
 <body class="bg-gym-bg min-h-screen">
     <x-app-header></x-app-header>
 </body>
